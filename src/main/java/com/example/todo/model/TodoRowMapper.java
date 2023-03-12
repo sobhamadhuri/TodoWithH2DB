bc4203e9de70e3fe2c4a32b2e4056a83b31da8d7
sobhamadhuri <majjisobhamadhuri@gmail.com>
2023-03-12 11:52:14 +0000

    solution

---
+++ src/main/java/com/example/todo/model/TodoRowMapper.java
@@ -0,0 +1,27 @@
+/*
+ * You can use the following import statements
+ *
+ * import org.springframework.beans.factory.annotation.Autowired;
+ * import org.springframework.web.bind.annotation.*;
+ * import java.util.*;
+ */
+
+// Write your code here
+package com.example.todo.model;
+
+import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.ResultSet;
+
+
+public class TodoRowMapper implements RowMapper<Todo>{
+
+    @Override
+    public Todo mapRow(ResultSet rs, int rowNum) throws SQLException{
+        return new Todo(
+            rs.getInt("id"),
+            rs.getString("todo"),
+            rs.getString("priority"),
+            rs.getString("status"));
+    }
+}
